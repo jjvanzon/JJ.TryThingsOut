9cd53831531b6ba920ccd8a74743764bd29b6eec
jjvanzon <janjoostvanzon@hotmail.com>
2018-07-30 21:18:23 +0200

    JJ.TryThingsOut.EntityFramework: * Make tests inconclusive upon database connection failure. * Copy some helpers from JJ.Framework for that. * Also get rid of (ReSharper) warnings.

---
+++ JJ.TryThingsOut.EntityFramework/TryEntityFramework.Tests/Helpers/ExceptionHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace TryEntityFramework.Tests.Helpers
+{
+	public static class ExceptionHelper
+	{
+		public static string FormatExceptionWithInnerExceptions(Exception ex, bool includeStackTrace)
+		{
+			var sb = new StringBuilder();
+			sb.AppendLine(FormatException(ex, includeStackTrace));
+
+			while (ex.InnerException != null)
+			{
+				sb.AppendLine("Inner exception:");
+				sb.AppendLine(FormatException(ex.InnerException, includeStackTrace));
+				ex = ex.InnerException;
+			}
+
+			return sb.ToString();
+		}
+
+		public static string FormatException(Exception ex, bool includeStackTrace)
+		{
+			if (ex == null) throw new ArgumentNullException(nameof(ex));
+
+			string message = ex.Message;
+			if (includeStackTrace)
+			{
+				message += Environment.NewLine + ex.StackTrace;
+			}
+			return message;
+		}
+
+        public static bool HasExceptionOrInnerExceptionsOfType<T>(Exception exception)
+        {
+            if (exception == null) throw new ArgumentNullException(nameof(exception));
+
+            bool any = exception.SelfAndAncestors(x => x.InnerException).OfType<T>().Any();
+            return any;
+        }
+	}
+}
--- JJ.TryThingsOut.EntityFramework/TryEntityFramework.Tests/Test_EntityFrameworkMappings.cs
+++ JJ.TryThingsOut.EntityFramework/TryEntityFramework.Tests/Test_EntityFrameworkMappings.cs
@@ -1,12 +1,11 @@
-﻿using System;
+﻿using System.Configuration;
+using System.Data.Entity;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TryEntityFramework.Model;
 using TryEntityFramework.Model.EntityFrameworkMappings;
-using System.Data.Entity;
-using System.Configuration;
-using System.Data.Entity.Infrastructure;
-using System.Data.Common;
-using System.Data.SqlClient;
+using TryEntityFramework.Tests.Helpers;
+
+// ReSharper disable UnusedVariable
 
 namespace TryEntityFramework.Tests
 {
@@ -15,33 +14,36 @@ namespace TryEntityFramework.Tests
     {
         [TestMethod]
         public void Test_EntityFrameworkMappings_GetObjects()
-        {
-            string connectionString = GetConnectionString_WithSpecialFormat();
-            using (DbContext context = new TryEntityFrameworkContext(connectionString))
-            {
-                foreach (Thing thing in context.Set<Thing>())
+            => AssertInconclusiveHelper.WithConnectionInconclusiveAssertion(
+                () =>
                 {
-                    string name = thing.Name;
-                }
-            }
-        }
+                    string connectionString = GetConnectionString_WithSpecialFormat();
+
+                    using (DbContext context = new TryEntityFrameworkContext(connectionString))
+                    {
+                        foreach (Thing thing in context.Set<Thing>())
+                        {
+                            string name = thing.Name;
+                        }
+                    }
+                });
 
         [TestMethod]
         public void Test_EntityFrameworkMappings_CreateObject()
-        {
-            string connectionString = GetConnectionString_WithSpecialFormat();
-            using (DbContext context = new TryEntityFrameworkContext(connectionString))
-            {
-                var thing = new Thing();
-                thing.Name = "Thing";
-                context.Set<Thing>().Add(thing);
-                context.SaveChanges();
-            }
-        }
+            => AssertInconclusiveHelper.WithConnectionInconclusiveAssertion(
+                () =>
+                {
+                    string connectionString = GetConnectionString_WithSpecialFormat();
+
+                    using (DbContext context = new TryEntityFrameworkContext(connectionString))
+                    {
+                        var thing = new Thing { Name = "Thing" };
+                        context.Set<Thing>().Add(thing);
+                        context.SaveChanges();
+                    }
+                });
 
         private string GetConnectionString_WithSpecialFormat()
-        {
-            return ConfigurationManager.ConnectionStrings["TryEntityFrameworkDBEntities"].ConnectionString;
-        }
+            => ConfigurationManager.ConnectionStrings["TryEntityFrameworkDBEntities"].ConnectionString;
     }
 }
