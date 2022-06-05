975561886833cc987d9312ecd310de641056eb2e
jjvanzon <janjoostvanzon@hotmail.com>
2022-06-05 21:09:42 +0200

    🥼 JJ.TryThingsOut.UnitTests2021

---
+++ JJ.TryThingsOut.UnitTests2021/JJ.TryThingsOut.UnitTests2021/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JJ.TryThingsOut.UnitTests2021")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JJ.TryThingsOut.UnitTests2021")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("66c3d916-692a-4c9b-872d-de3988dae379")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ JJ.TryThingsOut.UnitTests2021/JJ.TryThingsOut.UnitTests2021/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JJ.TryThingsOut.UnitTests2021
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int dummy = 1;
+            Assert.AreEqual(1, dummy);
+        }
+    }
+}
